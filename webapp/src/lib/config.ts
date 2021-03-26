af3f301bbbcbe97fb93452825d724d5b17a9d6cd
Christian Grigis <christian.grigis@epfl.ch>
2021-03-26 14:43:36 +0100

    Remove dynacred-related code

--- webapp/src/lib/config.ts
+++ webapp/src/lib/config.ts
@@ -1,14 +1,11 @@
 import { BEvmInstance } from "@dedis/cothority/bevm";
-import { ByzCoinRPC, IStorage } from "@dedis/cothority/byzcoin";
+import { ByzCoinRPC } from "@dedis/cothority/byzcoin";
 import Signer from "@dedis/cothority/darc/signer";
 import SignerEd25519 from "@dedis/cothority/darc/signer-ed25519";
 import Log from "@dedis/cothority/log";
 import { Roster } from "@dedis/cothority/network";
 
-import Dexie from "dexie";
 import toml from "toml";
-
-import { Fetcher, User } from "@c4dt/dynacred";
 
 import { StainlessRPC } from "src/lib/stainless";
 
@@ -16,33 +13,6 @@ const ROSTER_FILE = "conodes.toml";
 const STAINLESS_ROSTER_FILE = "conodes_stainless.toml";
 const BYZCOIN_CONFIG_FILE = "config.toml";
 const BEVM_CONFIG_FILE = "config_bevm.toml";
-
-/**
- * The main DB storage for dynacred users.
- */
-class StorageDB implements IStorage {
-    db: Dexie.Table<{ key: string, buf: Buffer }, string>;
-
-    constructor() {
-        const db = new Dexie("dynasent2");
-        db.version(1).stores({
-            contacts: "&key",
-        });
-        this.db = db.table("contacts");
-    }
-
-    async set(key: string, buf: Buffer) {
-        await this.db.put({key, buf});
-    }
-
-    async get(key: string): Promise<Buffer | undefined> {
-        const entry = await this.db.get({key});
-        if (entry !== undefined) {
-            return Buffer.from(entry.buf);
-        }
-        return undefined;
-    }
-}
 
 export class Config {
 
@@ -58,25 +28,12 @@ export class Config {
 
         const serverConfig = await Config.getServerConfig();
 
-        const db = new StorageDB();
-        const byzcoinRPC = await ByzCoinRPC.fromByzcoin(
-            roster, serverConfig.byzCoinID, undefined, undefined, undefined, db, false);
+        const byzcoinRPC = await ByzCoinRPC.fromByzcoin(roster, serverConfig.byzCoinID);
         const bevmInstance = await BEvmInstance.fromByzcoin(byzcoinRPC, serverConfig.bevmInstanceID);
-
-        let userData: User;
-        try {
-            const fetcher = new Fetcher(byzcoinRPC, db);
-            userData = await fetcher.retrieveUserByDB();
-        } catch (e) {
-            Log.lvl2("Cannot load DynaCred user data");
-            userData = null;
-        }
 
         let bevmUser: Signer;
         if (serverConfig.bevmUserID) {
             bevmUser = SignerEd25519.fromBytes(serverConfig.bevmUserID);
-        } else if (userData) {
-            bevmUser = userData.kiSigner;
         } else {
             throw new Error("Cannot determine bevmUser");
         }
